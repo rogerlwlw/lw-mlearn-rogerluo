6f9b3cac5dd2f736442fe2dc368f5b7f776969f1
rogerlwlw <42911353+rogerlwlw@users.noreply.github.com>
2019-03-27 19:28:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,27 +14,26 @@ for line in open('lw_mlearn/__init__.py'):
     match = re.match("__version__ *= *'(.*)'", line)
     if match:
         __version__, = match.groups()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
                
 setup(
-    name="lw-mlearn",
+    name="lw-mlearn-rogerluo",
     version=__version__,
     packages=find_packages(include=['lw_mlearn']),
     install_requires=['sklearn-pandas', 
                       'scikit-learn', 
                       'pandas', 
-                      'yapf'],
-    description="This is a machine learning Package based on sklearn estimator",   
-    # metadata to display on PyPI
+                      'yapf',
+                      'xgboost'],
+    description="This is a machine learning Package based on sklearn estimator", 
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
     author="rogerluo",
-    author_email="coolww@outlook.com", 
-    
+    author_email="coolww@outlook.com",     
     license="PSF",
     keywords=['Machine learning', 'sklearn', 'pandas'],
     url= 'https://github.com/rogerlwlw/lw-mlearn'
     # could also include long_description, download_url, classifiers, etc.
 )
-
-
-
-
-
