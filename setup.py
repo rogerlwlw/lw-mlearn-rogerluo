f25822bef6d8a9907d099926dfa7c685999e795c
rogerlwlw <42911353+rogerlwlw@users.noreply.github.com>
2019-03-28 15:09:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ Created on Mon Mar 25 19:01:45 2019
 import re
 from setuptools import setup, find_packages
 
-
 for line in open('lw_mlearn/__init__.py'):
     match = re.match("__version__ *= *'(.*)'", line)
     if match:
@@ -22,18 +21,34 @@ setup(
     name="lw-mlearn-rogerluo",
     version=__version__,
     packages=find_packages(),
-    install_requires=['sklearn-pandas', 
-                      'scikit-learn', 
-                      'pandas', 
-                      'matplotlib',
-                      'yapf',
-                      'xgboost'],
-    description="This is a machine learning Package based on sklearn estimator", 
-    long_description=long_description, 
-    long_description_content_type="text/markdown",
+    inlcude_package_data=True,    
+    install_requires=['sklearn-pandas',
+                      'numpy>=1.15.0', 
+                      'pandas>=0.23.0',
+                      'scikit-learn>=0.20.0', 
+                      'scipy>=1.1.0',
+                      'matplotlib>=3.0.0',
+                      'yapf>=0.25.0',
+                      'xgboost>=0.81'],
     author="rogerluo",
-    author_email="coolww@outlook.com",     
-    license="PSF",
-    keywords=['Machine learning', 'sklearn', 'pandas'],
-    url= 'https://github.com/rogerlwlw/lw-mlearn'
+    author_email="coolww@outlook.com",  
+    description="Machine learning automatic workflow based on sklearn estimator", 
+    long_description=long_description, 
+    long_description_content_type="text/markdown",   
+    license='GNU/LGPLv3',
+    url= 'https://github.com/rogerlwlw/lw-mlearn-rogerluo',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    keywords=['pipeline optimization', 'hyperparameter optimization', 
+              'data science', 'machine learning', 'genetic programming', 
+              'evolutionary computation'],
+
 )
