f89bea2884bd8ebbbe2032e7246c723bca12bf97
rogerlwlw <42911353+rogerlwlw@users.noreply.github.com>
2019-04-08 18:20:11 +0800

    Update __init__.py

--- lw_mlearn/__init__.py
+++ lw_mlearn/__init__.py
@@ -4,6 +4,6 @@ __version__ = '0.0.1'
 __all__ = ['lw_model', 'lw_preprocess']
 
 from . import *
-from . lw_model import ML_model
-from . yapf_fmt import yapf_allfile
-from . lw_preprocess import pipe_main
+from .lw_model import ML_model
+from .yapf_fmt import yapf_allfile
+from .lw_preprocess import pipe_main
