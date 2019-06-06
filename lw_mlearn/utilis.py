ef1a98370f8f00541ceefed91c0b163544e46c60
rogerlwlw <42911353+rogerlwlw@users.noreply.github.com>
2019-06-06 15:54:27 +0800

    Update utilis.py

--- lw_mlearn/utilis.py
+++ lw_mlearn/utilis.py
@@ -107,3 +107,22 @@ def dec_iferror_getargs(func):
                 func.__name__))
 
     return wrapper
+
+class Substitution(object):
+    """Decorate a function's or a class' docstring to perform string
+    substitution on it.
+
+    This decorator should be robust even if obj.__doc__ is None
+    (for example, if -OO was passed to the interpreter)
+    """
+
+    def __init__(self, *args, **kwargs):
+        if (args and kwargs):
+            raise AssertionError("Only positional or keyword args are allowed")
+
+        self.params = args or kwargs
+
+    def __call__(self, obj):
+        obj.__doc__ = obj.__doc__.format(**self.params)
+        return obj
+
