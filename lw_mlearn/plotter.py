415f48c6fac1beaed311361e17f685a02ed6f0d5
rogerlwlw <42911353+rogerlwlw@users.noreply.github.com>
2019-06-19 18:30:15 +0800

    Update plotter.py

--- lw_mlearn/plotter.py
+++ lw_mlearn/plotter.py
@@ -18,38 +18,46 @@ from pandas.core.dtypes import api
 from scipy import interp
 from sklearn.metrics import auc, roc_curve
 
-from . utilis import get_flat_list, get_kwargs
+from .utilis import get_flat_list, get_kwargs
 
 plt.style.use('seaborn')
 plt.rcParams.update({
-    'figure.dpi': 90.0,
-    'axes.unicode_minus' : False, 
-    'font.family' : ['sans-serif'],
-    'font.sans-serif': [ 
-             'Arial',
-             'Liberation Sans',
-             'DejaVu Sans',
-             'Bitstream Vera Sans',
-             'sans-serif'
-             'Microsoft YaHei',
-             'Microsoft JhengHei',
-             ],       
-    'font.size': 10.0,
-    'font.style': 'normal',
-    'font.weight': 'semibold',
+    'figure.dpi':
+    90.0,
+    'axes.unicode_minus':
+    False,
+    'font.family': ['sans-serif'],
+    'font.sans-serif': [
+        'Arial',
+        'Liberation Sans',
+        'DejaVu Sans',
+        'Bitstream Vera Sans',
+        'sans-serif'
+        'Microsoft YaHei',
+        'Microsoft JhengHei',
+    ],
+    'font.size':
+    10.0,
+    'font.style':
+    'normal',
+    'font.weight':
+    'semibold',
 })
+
 
 def txt_fontdict(**kwargs):
     '''
     '''
-    font = {'family' : 'serif',
-            'color'  : 'black',
-            'weight' : 'normal',
-            'size'   : 12,
-        }
+    font = {
+        'family': 'serif',
+        'color': 'black',
+        'weight': 'normal',
+        'size': 12,
+    }
     font.update(**kwargs)
     print(font)
     return font
+
 
 def plotter_auc(fpr,
                 tpr,
@@ -94,14 +102,13 @@ def plotter_auc(fpr,
             ks_score = max(np.array(tpr[i]) - np.array(fpr[i]))
             aucs.append(auc_score)
             kss.append(ks_score)
-            ax.plot(
-                fpr[i],
-                tpr[i],
-                color=cmlist[i],
-                alpha=alpha,
-                lw=lw,
-                label='ROC %r (AUC=%0.2f;KS=%0.2f)' % (names[i], auc_score,
-                                                       ks_score))
+            ax.plot(fpr[i],
+                    tpr[i],
+                    color=cmlist[i],
+                    alpha=alpha,
+                    lw=lw,
+                    label='ROC %r (AUC=%0.2f;KS=%0.2f)' %
+                    (names[i], auc_score, ks_score))
     # plot mean tpr line
     if n > 1:
         mean_fpr = np.linspace(0, 1, 100)
@@ -111,36 +118,33 @@ def plotter_auc(fpr,
         mean_tpr[-1] = 1.0
         mean_auc = np.mean(aucs)
         std_auc = np.std(aucs)
-        ax.plot(
-            mean_fpr,
-            mean_tpr,
-            'b-.',
-            alpha=1,
-            lw=1.5,
-            label='Mean ROC(AUC=%0.2f $\pm$ %0.2f)' % (mean_auc, std_auc))
+        ax.plot(mean_fpr,
+                mean_tpr,
+                'b-.',
+                alpha=1,
+                lw=1.5,
+                label='Mean ROC(AUC=%0.2f $\pm$ %0.2f)' % (mean_auc, std_auc))
         #plot variance
         std_tpr = np.std(tprs, axis=0)
         tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
         tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-        ax.fill_between(
-            mean_fpr,
-            tprs_lower,
-            tprs_upper,
-            color='grey',
-            alpha=.3,
-            label=r'$\pm$ 1 std. dev.')
+        ax.fill_between(mean_fpr,
+                        tprs_lower,
+                        tprs_upper,
+                        color='grey',
+                        alpha=.3,
+                        label=r'$\pm$ 1 std. dev.')
     # plot chance line
     ax.plot([0, 1], [0, 1], 'k--', lw=1.5, label='Chance (AUC=0.5)')
     # set property
     if title is None:
         title = 'Receiver operating characteristic'
-    plt.setp(
-        ax,
-        xlabel='False Positive Rate',
-        ylabel='True Positive Rate',
-        xlim=[-0.05, 1.05],
-        ylim=[-0.05, 1.05],
-        title=title)
+    plt.setp(ax,
+             xlabel='False Positive Rate',
+             ylabel='True Positive Rate',
+             xlim=[-0.05, 1.05],
+             ylim=[-0.05, 1.05],
+             title=title)
     plt.legend(loc="lower right", fontsize='medium', bbox_to_anchor=(1, 0))
     plt.tight_layout()
     return ax
@@ -161,7 +165,7 @@ def plotKS(y_pred, y_true, n, asc):
     # preds is prob: asc=0
 
     pred = y_pred  # 预测值
-    bad = y_true # 取1为bad, 0为good
+    bad = y_true  # 取1为bad, 0为good
     ksds = pd.DataFrame({'bad': bad, 'pred': pred})
     ksds['good'] = 1 - ksds.bad
 
@@ -206,8 +210,8 @@ def plotKS(y_pred, y_true, n, asc):
     ksds = ksds[['tile', 'cumsum_good', 'cumsum_bad', 'ks']]
     ksds0 = np.array([[0, 0, 0, 0]])
     ksds = np.concatenate([ksds0, ksds], axis=0)
-    ksds = pd.DataFrame(
-        ksds, columns=['tile', 'cumsum_good', 'cumsum_bad', 'ks'])
+    ksds = pd.DataFrame(ksds,
+                        columns=['tile', 'cumsum_good', 'cumsum_bad', 'ks'])
 
     ks_value = ksds.ks.max()
     ks_pop = ksds.tile[ksds.ks.idxmax()]
@@ -215,44 +219,40 @@ def plotKS(y_pred, y_true, n, asc):
            ' at pop = ' + str(np.round(ks_pop, 4)))
 
     # chart
-    plt.plot(
-        ksds.tile,
-        ksds.cumsum_good,
-        label='cum_good',
-        color='blue',
-        linestyle='-',
-        linewidth=2)
+    plt.plot(ksds.tile,
+             ksds.cumsum_good,
+             label='cum_good',
+             color='blue',
+             linestyle='-',
+             linewidth=2)
 
-    plt.plot(
-        ksds.tile,
-        ksds.cumsum_bad,
-        label='cum_bad',
-        color='red',
-        linestyle='-',
-        linewidth=2)
+    plt.plot(ksds.tile,
+             ksds.cumsum_bad,
+             label='cum_bad',
+             color='red',
+             linestyle='-',
+             linewidth=2)
 
-    plt.plot(
-        ksds.tile,
-        ksds.ks,
-        label='ks',
-        color='green',
-        linestyle='-',
-        linewidth=2)
+    plt.plot(ksds.tile,
+             ksds.ks,
+             label='ks',
+             color='green',
+             linestyle='-',
+             linewidth=2)
 
     plt.axvline(ks_pop, color='gray', linestyle='--')
     plt.axhline(ks_value, color='green', linestyle='--')
-    plt.axhline(
-        ksds.loc[ksds.ks.idxmax(), 'cumsum_good'],
-        color='blue',
-        linestyle='--')
-    plt.axhline(
-        ksds.loc[ksds.ks.idxmax(), 'cumsum_bad'], color='red', linestyle='--')
-    plt.title(
-        'KS=%s ' % np.round(ks_value, 4) + 'at Pop=%s' % np.round(ks_pop, 4),
-        fontsize=15)
+    plt.axhline(ksds.loc[ksds.ks.idxmax(), 'cumsum_good'],
+                color='blue',
+                linestyle='--')
+    plt.axhline(ksds.loc[ksds.ks.idxmax(), 'cumsum_bad'],
+                color='red',
+                linestyle='--')
+    plt.title('KS=%s ' % np.round(ks_value, 4) +
+              'at Pop=%s' % np.round(ks_pop, 4),
+              fontsize=15)
 
     return ksds
-
 
 
 def plotter_cv_results_(results,
@@ -295,13 +295,12 @@ def plotter_cv_results_(results,
         std = df.pop('std_test_' + s)
         x = df.index.get_values()
         df.plot.line(style=[train_style, test_style], ax=ax0)
-        ax0.fill_between(
-            x,
-            mean - std,
-            mean + std,
-            color='grey',
-            alpha=.2,
-            label=r'$\pm$ 1 std. dev.')
+        ax0.fill_between(x,
+                         mean - std,
+                         mean + std,
+                         color='grey',
+                         alpha=.2,
+                         label=r'$\pm$ 1 std. dev.')
         # annotate
         x_max = df.index[np.argmax(mean)]
         best_score = np.max(mean)
@@ -312,17 +311,16 @@ def plotter_cv_results_(results,
             ['score_max= %0.4f $\pm$ %0.2f' % (np.max(mean), np.mean(std))])
         ax0.axvline(x_max, linestyle='--', marker='x', color='y')
         ax0.annotate("%0.4f" % best_score, (x_max, best_score))
-        ax0.set_xlim(x.min()-0.5, x.max()+0.5)
+        ax0.set_xlim(x.min() - 0.5, x.max() + 0.5)
         plt.setp(ax0, ylabel=s)
 
     # set title
     ax[0].set_title(title, fontsize=13)
     # use fig legend
-    fig.legend(
-        h, ('train', 'test', r'$\pm$ 1 std. dev.'),
-        loc='upper right',
-        ncol=3,
-        bbox_to_anchor=(0.98, 1))
+    fig.legend(h, ('train', 'test', r'$\pm$ 1 std. dev.'),
+               loc='upper right',
+               ncol=3,
+               bbox_to_anchor=(0.98, 1))
     ax[-1].set_xlabel(xlabel)
     plt.tight_layout(rect=(0, 0, 1, 0.95))
     return ax
@@ -379,13 +377,12 @@ def plotter_rateVol(df,
 
     # set axe attr
     fmt = get_ticks_formatter(ymajor_formatter)
-    plt.setp(
-        axe,
-        ylabel=rate.name,
-        xlabel=labels.name,
-        xticklabels=labels,
-        ylim=ylim)
-    
+    plt.setp(axe,
+             ylabel=rate.name,
+             xlabel=labels.name,
+             xticklabels=labels,
+             ylim=ylim)
+
     axe.yaxis.set_major_formatter(fmt)
     axe.xaxis.set_label_position(xlabel_position)
     if labels.astype(str).apply(len).max() > 8:
@@ -395,12 +392,12 @@ def plotter_rateVol(df,
     axe_right.grid(False)
 
     bbox = dict(boxstyle="round", fc='w', alpha=1)
-    axe.annotate(
-        'mean rate=%-0.2f%%(vol=%d)' % (rate_weighted * 100, sum(vol)),
-        (0, rate_weighted), (0.01, 0.95),
-        xycoords='data',
-        textcoords='axes fraction',
-        bbox=bbox)
+    axe.annotate('mean rate=%-0.2f%%(vol=%d)' %
+                 (rate_weighted * 100, sum(vol)), (0, rate_weighted),
+                 (0.01, 0.95),
+                 xycoords='data',
+                 textcoords='axes fraction',
+                 bbox=bbox)
     if anno is True:
         _annotate(rate.index.values, rate.values, axe)
     # get legends
@@ -408,6 +405,7 @@ def plotter_rateVol(df,
     plt.tight_layout(pad=1.08, rect=(0, 0, 1, 0.98))
 
     return out
+
 
 def get_ticks_formatter(name, *args, **kwargs):
     ''' return ticks formattor  
@@ -447,14 +445,13 @@ def plotter_score_path(df_score, title=None, cm=None, style='-.o'):
         if api.is_numeric_dtype(s):
 
             s.plot(ax=ax0, color=cmlist[i], style=style)
-            ax0.fill_between(
-                s.index,
-                s - s.std(),
-                s + s.std(),
-                color='grey',
-                alpha=.3,
-                label=r'{} = {}$\pm$ {}'.format(col, round(s.mean(), 4),
-                                                round(s.std(), 4)))
+            ax0.fill_between(s.index,
+                             s - s.std(),
+                             s + s.std(),
+                             color='grey',
+                             alpha=.3,
+                             label=r'{} = {}$\pm$ {}'.format(
+                                 col, round(s.mean(), 4), round(s.std(), 4)))
             plt.setp(ax0, ylabel=col)
             h, l = ax0.get_legend_handles_labels()
             ax0.legend([h[-1]], [l[-1]])
@@ -465,17 +462,15 @@ def plotter_score_path(df_score, title=None, cm=None, style='-.o'):
     return fig
 
 
-
 def _annotate(x, y, ax):
     ''' plot annotate
     '''
     if api.is_list_like(x):
         for i, j in zip(x, y):
-            ax.annotate(
-                s='%.1f%%' % (100 * j),
-                xy=(i, j),
-                xytext=(10, 10),
-                textcoords='offset pixels')
+            ax.annotate(s='%.1f%%' % (100 * j),
+                        xy=(i, j),
+                        xytext=(10, 10),
+                        textcoords='offset pixels')
     return ax
 
 
@@ -494,8 +489,8 @@ def make_meshgrid(x, y, h=.02):
     """
     x_min, x_max = x.min() - 1, x.max() + 1
     y_min, y_max = y.min() - 1, y.max() + 1
-    xx, yy = np.meshgrid(
-        np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
     return xx, yy
 
 
@@ -528,4 +523,3 @@ def plotter_contours(ax,
     Z = Z.reshape(xx.shape)
     out = ax.contourf(xx, yy, Z, **params)
     return out
-
