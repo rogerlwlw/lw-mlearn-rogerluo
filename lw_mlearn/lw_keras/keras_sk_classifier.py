fe5c43b2fce0b9540c0485457afa182011445f7a
rogerlwlw <42911353+rogerlwlw@users.noreply.github.com>
2019-06-19 18:30:07 +0800

    Update keras_sk_classifier.py

--- lw_mlearn/lw_keras/keras_sk_classifier.py
+++ lw_mlearn/lw_keras/keras_sk_classifier.py
@@ -7,7 +7,8 @@ Created on Wed Jun  5 12:54:01 2019
 from functools import wraps
 from keras.wrappers.scikit_learn import KerasClassifier
 
-from . mlp import  bfn_dense
+from .mlp import bfn_dense
+
 
 class KerasClassifier_(KerasClassifier):
     '''wrap keras model as scikit estimators
@@ -17,7 +18,6 @@ class KerasClassifier_(KerasClassifier):
     def fit(self, X, y=None, sample_weight=None, **kwargs):
         '''
         rewrite fit method to check input shape of X
-        '''        
-        self.set_params(input_shape = X.shape[1:])
-        return super().fit(X, y, sample_weight,
-                     **kwargs)
+        '''
+        self.set_params(input_shape=X.shape[1:])
+        return super().fit(X, y, sample_weight, **kwargs)
