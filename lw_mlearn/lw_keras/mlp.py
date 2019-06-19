da1bf8c8c3ffcaf431ced78127f158294b0b2361
rogerlwlw <42911353+rogerlwlw@users.noreply.github.com>
2019-06-19 18:30:10 +0800

    Update mlp.py

--- lw_mlearn/lw_keras/mlp.py
+++ lw_mlearn/lw_keras/mlp.py
@@ -10,25 +10,26 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras import regularizers as reg
 
-from . metrics_c import get_metrics
-from . docstring import Substitution
-from . keras_doc_string import Keras_doc_
+from .metrics_c import get_metrics
+from .docstring import Substitution
+from .keras_doc_string import Keras_doc_
 
-      
-@Substitution(optimizer=Keras_doc_.optimizer.strip(), loss=Keras_doc_.loss.strip(), 
+
+@Substitution(optimizer=Keras_doc_.optimizer.strip(),
+              loss=Keras_doc_.loss.strip(),
               metrics=Keras_doc_.metrics.strip(),
-              input_shape=Keras_doc_.input_shape.strip()) 
-def bfn_dense( dense_layer=(1,), 
-               dropout=(0,), 
-               input_shape=(32,),
-               out_activation='sigmoid', 
-               hidden_activation='sigmoid', 
-               optimizer='adam',
-               loss='binary_crossentropy', 
-               metrics=['binary_accuracy'],   
-               penalty='l1',
-               dropout_ratio=0.2,
-               **kwargs):
+              input_shape=Keras_doc_.input_shape.strip())
+def bfn_dense(dense_layer=(1, ),
+              dropout=(0, ),
+              input_shape=(32, ),
+              out_activation='sigmoid',
+              hidden_activation='sigmoid',
+              optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['binary_accuracy'],
+              penalty='l1',
+              dropout_ratio=0.2,
+              **kwargs):
     '''build function to return densely connected network
     
     parameters
@@ -51,33 +52,39 @@ def bfn_dense( dense_layer=(1,),
     keras muli-layer perceptron model compiled  
     '''
     model = Sequential()
-    
-    regularizer = { 'l1' : reg.l1(), 'l2' : reg.l2(), 'l1_l2' : reg.l1_l2()}
- 
+
+    regularizer = {'l1': reg.l1(), 'l2': reg.l2(), 'l1_l2': reg.l1_l2()}
+
     layer_structure = []
     for i, item in enumerate(dense_layer):
         if i < len(dropout):
             layer_structure.append((item, dropout[i]))
         else:
             layer_structure.append(item, 0)
-                   
+
     n = 0
     for i, j in layer_structure:
         n += 1
         if n == 1:
-            model.add(Dense(i, input_shape=input_shape, 
-                            activation=hidden_activation,
-                            kernel_regularizer=regularizer.get(penalty)))
+            model.add(
+                Dense(i,
+                      input_shape=input_shape,
+                      activation=hidden_activation,
+                      kernel_regularizer=regularizer.get(penalty)))
         elif n == len(dense_layer):
-            model.add(Dense(i, activation=out_activation,
-                            kernel_regularizer=regularizer.get(penalty)))           
+            model.add(
+                Dense(i,
+                      activation=out_activation,
+                      kernel_regularizer=regularizer.get(penalty)))
         else:
-            model.add(Dense(i, activation=hidden_activation,
-                            kernel_regularizer=regularizer.get(penalty)))
-        if bool(j):            
+            model.add(
+                Dense(i,
+                      activation=hidden_activation,
+                      kernel_regularizer=regularizer.get(penalty)))
+        if bool(j):
             model.add(Dropout(dropout_ratio))
-            
-    metrics = [get_metrics(i) for i in metrics]      
+
+    metrics = [get_metrics(i) for i in metrics]
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-        
+
     return model
