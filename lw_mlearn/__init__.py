6e6f1a433785f0acd42f91c06aa8a98434763ea4
rogerlwlw <42911353+rogerlwlw@users.noreply.github.com>
2019-06-20 11:54:06 +0800

    Update __init__.py

--- lw_mlearn/__init__.py
+++ lw_mlearn/__init__.py
@@ -1,6 +1,7 @@
 #
 from . lw_model import ML_model, train_models, model_experiments
-from . lw_preprocess import pipe_main, pipe_grid
+from . lw_preprocess import pipe_main, pipe_grid, plotter_lift_curve
+from . plotter import plotter_auc_y
 
 
 __version__ = '0.0.1'
