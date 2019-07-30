cccc2f09c24fb7e556ba4b5ad06f9356d05fc765
rogerlwlw <coolww@outlook.com>
2019-07-30 15:36:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,27 +12,31 @@ import os
 def test_model():
     '''with small fake data to walk through workflow
     '''
-    X, y = make_classification(300, n_redundant=5, n_features=30)
+    X, y = make_classification(300, n_redundant=5, n_features=50)
     l = [
-        'clean_oht_LDA_fxgb_cleanNN_AdaBoostClassifier',
-        'clean_oht_fxgb_RUSBoostClassifier',
-        'clean_oht_LDA_cleanNN_stdscale_GaussianProcessClassifier',
-        'clean_oht_fRFErf_cleanNN_stdscale_GaussianProcessClassifier',
-        'clean_oht_fRFErf_cleanNN_stdscale_SVC',
-        'clean_oht_LDA_cleanNN_stdscale_SVC',
-        'clean_oht_fxgb_cleanNN_XGBClassifier',
-        'clean_oht_fxgb_oside_XGBClassifier',
-        'clean_oht_frf_oside_XGBClassifier',
-        'clean_oht_fxgb_cleanNN_RandomForestClassifier',
-        'clean_oht_frf_RandomForestClassifier',
-        'clean_oht_fxgb_BalancedRandomForestClassifier',
-        'clean_oht_fxgb_cleanNN_GradientBoostingClassifier',
-        'clean_oht_fRFElog_cleanNN_GradientBoostingClassifier',
-        'clean_oht_fxgb_cleanNN_DecisionTreeClassifier',
+            'cleanNA_woe_frf_LogisticRegression',
+            'cleanNA_woe_frf20_LogisticRegression',
+            'cleanNA_woe_cleanNN_fRFErf_LogisticRegression',
+            'cleanNA_woe_frf10_LogisticRegression',
+            'cleanMean_woe_frf20_Nys100_SGDClassifier',
+            'cleanMean_oht_stdscale_frf20_Nys_SGDClassifier',
+            'clean_oht_XGBClassifier',
+            'clean_oht_oside_frf_XGBClassifier',
+            'clean_oht_cleanNN_fxgb_XGBClassifier',
+            'clean_oht_cleanNN_inlierForest_fxgb_XGBClassifier',
+            'clean_oht_frf_RandomForestClassifier',
+            'clean_oht_fxgb_BalancedRandomForestClassifier',
+            'clean_oht_cleanNN_frf_AdaBoostClassifier',
+            'clean_oht_fxgb_RUSBoostClassifier',
+            'clean_oht_fxgb_cleanNN_GradientBoostingClassifier',
+            'clean_oht_fsvm_cleanNN_GradientBoostingClassifier',
+            'clean_oht_fxgb_cleanNN_DecisionTreeClassifier',
     ]
     # --
     for i in l:
-        model = train_models(i, (X, y), (X, y), max_leaf_nodes=10)
+        model = train_models(i, (X, y), (X, y),
+                             max_leaf_nodes=10,
+                             scoring=['KS', 'roc_auc'])
         model.delete_model()
 
 
