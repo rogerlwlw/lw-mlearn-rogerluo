6a59c40dc98737dd64e1305198a0f64924be4892
rogerlwlw <coolww@outlook.com>
2019-08-12 18:52:40 +0800

    Update build_ensemble.py

--- lw_mlearn/lw_mlens/build_ensemble.py
+++ lw_mlearn/lw_mlens/build_ensemble.py
@@ -11,14 +11,51 @@ import numpy as np
 from mlens.ensemble import SuperLearner, Subsemble, BlendEnsemble
 from mlens.preprocessing import Subset
 
-def build_stack(estimators, meta_estimator, propagate_features=None, 
-                proba=True):
+from lw_mlearn.utilis.utilis import get_kwargs, get_flat_list
+
+
+from functools import wraps
+from matplotlib import docstring
+
+def get_score_fn(scoring):
     '''
-    
-    '''
-    
-    
-    if meta_estimator is not None:
-        pass
+    scoring: str or callable
     
     return
+    ----
+    scoring function that accepts an array of true values and an array 
+    of predictions: score = f(y_true, y_pred).      
+    '''
+    if callable(scoring):
+        return scoring
+    else:
+        return 
+    
+@docstring.Appender(SuperLearner.add.__doc__)
+@wraps(SuperLearner)
+def build_super(
+             estimators,
+             preprocessing,
+             proba,
+             meta, 
+             propagate_features,                       
+             folds=3,
+             shuffle=True,
+             random_state=0,
+             
+             scorer='roc_auc',
+             raise_on_exception=True,
+             n_jobs=2,
+             model_selection=False,
+             sample_size=20):
+    '''return stack ensemble model instance
+    
+    params 
+    -------
+    see __doc__ of SuperLearner & SuperLearner.add, already wrapped in
+    '''
+    scorer = get_score_fn(scorer) # return scoring functions
+    L = locals().copy()
+    # --
+    ens = SuperLearner().add()
+    return ens
