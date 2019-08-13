9bd01ee6d81f6857d44d1baddba734da72641dbc
rogerlwlw <coolww@outlook.com>
2019-08-13 14:52:30 +0800

    Update __init__.py

--- lw_mlearn/__init__.py
+++ lw_mlearn/__init__.py
@@ -3,8 +3,8 @@ machine learning based on sklearn-estimators
 
 @author: rogerluo
 '''
-from lw_model import (ML_model, train_models, run_analy, run_CVscores)
-from lw_preprocess import pipe_main, pipe_grid, plotter_lift_curve
+from . lw_model import ML_model, run_analy, run_CVscores
+from . lw_preprocess import pipe_main, pipe_grid, plotter_lift_curve
 
 
 __version__ = '0.0.1'
